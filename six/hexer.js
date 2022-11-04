9deb081f9c09dcf1183a59986576b3abcd855777
Detmar Peterke <detmar@peterke.com>
2022-11-04 09:42:53 -0700

    Working on face mapping on the sphere

--- six/hexer.js
+++ six/hexer.js
@@ -16,9 +16,9 @@ export class Hexer {
       Its basic shape is this:
 
         Vertices -- think of these at the pentagons on the sphere:
-                0   +   +   +   +
-              2   4   6   8   10  +
-            1*  3   5   7   9  +
+               10   +   +   +   +
+              1   3   5   7   9   +
+            0*  2   4   6   8  +
               11  +   +   +   +
 
         Faces -- think of these as composed of tiles of hexagons:
@@ -43,6 +43,9 @@ export class Hexer {
       [1,-1],
     ];
     let h = Math.sqrt(3) / 2;
+
+    this.korners = this._makeCorners();
+
     this.schematic = {};
 
     let r;  // radius of "unit" axis
@@ -65,6 +68,16 @@ export class Hexer {
     this.corners.forEach(v => {
       this._appendOffset(this.schematic.corners, dx, dy, _vScale(v, e));
     });
+
+    // [ ] define dx, dy, in terms of how to get to the other corners
+    // [ ] define up and down arrays
+
+    // dx: points to the next corner to the east
+    // dy: points to the next corner to the north-east
+    // edge: is the distance to those corners
+    this.schematic.dx = dx;
+    this.schematic.dy = dy;
+    this.schematic.edge = e;
 
     this.schematic.ups = [];
 
@@ -96,7 +109,7 @@ export class Hexer {
       for (let j = 0; j > -top; j--) {
         for (let i = (-j % 3) - j; i < top; i += 3) {
           if (i || j) {
-            this._appendOffset(this.schematic.ups, dx, dy, [i, j]);
+            this._appendOffset(this.schematic.downs, dx, dy, [i, j]);
           }
         }
       }
@@ -118,6 +131,27 @@ export class Hexer {
         this.schematic.corners[c]);
     }
 
+    // coords for each center face
+    let verts = [[0, 1, 0],]; // north pole
+    let spherical = [[Math.PI / 2, 0]];
+
+    let lat = Math.atan(0.5);
+    for (let i = 0; i < 10; i++) {
+      let theta = (i % 2) ? lat : -lat;
+      let phi = Math.PI * (i - 5) / 5;
+      spherical.push([theta, phi]);
+      verts.push(this._fromSpherical([theta, phi]));
+    }
+
+    verts.push([0,-1,0]); // south pole
+    spherical.push([-Math.PI / 2, 0]);
+
+    this.sphere = {
+      verts: verts,
+      spherical: spherical,
+    };
+
+    // some stats
     let stats = {
       n:n,
       corners:12,
@@ -145,31 +179,106 @@ export class Hexer {
     face.forEach(v => {
       list.push(_vAdd(v, offset));
     });
-
-    let verts = [[0, 1, 0],]; // north pole
-    let lat = Math.atan(0.5);
-    for (let i = 0; i < 10; i++) {
-      let phi = Math.PI * (i - 5) / 5;
-      let theta = (i % 2) ? lat : -lat;
-      verts.push(this._fromSpherical(theta, phi));
-    }
-    verts.push([0,-1,0]); // south pole
-    this.sphere = {
-      verts:verts
-    };
   }
 
-  _fromSpherical(lat, long) {
+  _fromSpherical(spherical) {
+    let [long, lat] = spherical;
     // lat: radians, [-pi/2,pi/2], 0 equator, pi/2 north pole
     // long: radians, [-pi,pi], 0 england, neg america, pos asia.
     // Return [x,y,z] on unit sphere;
-    //   Y-axis: North pole
     //   X-axis: Gulf of Guinea
+    //   Y-axis: North pole
     //   Z-axis: Galapagos
     return [
       Math.cos(lat) * Math.cos(-long),
       Math.sin(lat),
       Math.cos(lat) * Math.sin(-long),
     ];
+  }
+
+  _makeCorners() {
+    // The corners are logically the 12 pentagons on the tiled sphere.
+    // They are the 12 vertices of a icosahedron.
+    // Make a corners array, and return it.
+    let corners = [];
+    for (let i = 0; i < 12; i++) {
+      // For each corner index;
+      let c = {
+        ci: i, // the Corner index
+        neighborCi: [],
+        even: !(i % 2),
+      };
+
+      if (i < 10) {
+        // one of the 10 belt corners
+        c.belt = true;
+        // neighbors
+        const template = c.even ? [11,2,1,-1,-2] : [10,-2,-1,1,2];
+        for (let j = 0; j < 5; j++) {
+          let n = template[j];
+          if (j) {
+            n = (n + i + 10) % 10;
+          }
+          c.neighborCi.push(n);
+        }
+        if (c.even) {
+          c.upCi = i + 1;
+          c.downCi = 11;
+        }
+        else {
+          c.upCi = 10;
+          c.downCi = (i + 1) % 10;
+        }
+        c.rightCi = (i + 2) % 10;
+
+        let lat = Math.atan(0.5);
+        let theta = c.even ? -lat : lat;
+        let phi = Math.PI * (i - 5) / 5;
+        c.spherical = [phi, theta];
+      }
+      else {
+        // the poles
+        c.pole = true;
+        c.neighborCi = ((i == 10) ? [1,3,5,7,9] : [8,6,4,2,0]);
+
+        let phi = Math.PI / ((i == 10) ? 2 : -2);
+        c.spherical = [0, phi, true];
+      }
+      corners.push(c);
+    }
+
+    return corners;
+  }
+
+  lerpPatch(a, b, c, fx, fy) {
+    /*
+      Find the spherical coordinates on a triangular patch of the sphere.
+        b
+      /
+     a----c
+     <a,b,c> is a triangle on the unit sphere, using my spherical coords.
+     fx is a fraction along [a,c]
+     fy is a fraction along [a,b]
+     Returns spherical coordinate list [theta, phi]
+    */
+    let dx = [c[0]-a[0], c[1]-a[1]];
+    let dy = [b[0]-a[0], b[1]-a[1]];
+    if (dx[0] < 0) {
+      // dx wrap-around case
+      dx[0] += Math.PI * 2;
+    }
+    if (b[0] < (a[0] - 3)) {
+      // dy wrap-around case
+      dy[0] += Math.PI * 2;
+    }
+    if (b[2]) {
+      // b is degenerate (is a pole)
+      dy[0] = 0;
+    }
+    let f = [
+      (dx[0]*fx + dy[0]*fy) + a[0],
+      (dx[1]*fx + dy[1]*fy) + a[1],
+    ];
+    return f;
   }
 }
--- six/part1.js
+++ six/part1.js
@@ -23,7 +23,19 @@ export class Part1 {
       this.addDot(c, 1, [0x004488, 0x0077ff]);
     });
 
-    let sphere = this.makeSchematicSphere(this.hexer);
+    let xx = [
+      this.hexer.schematic.dx[0] * this.hexer.schematic.edge,
+      this.hexer.schematic.dx[1] * this.hexer.schematic.edge,
+    ];
+    let yy = [
+      this.hexer.schematic.dy[0] * this.hexer.schematic.edge,
+      this.hexer.schematic.dy[1] * this.hexer.schematic.edge,
+    ];
+    this.addDot(xx, 1, [0x880000, 0xaa0000]);
+    this.addDot(yy, 1, [0x880000, 0xaa0000]);
+
+    let sphere = this.make2(this.hexer);
+    //let sphere = this.makeSchematicSphere(this.hexer);
     this.group.add(sphere);
   }
 
@@ -65,6 +77,69 @@ export class Part1 {
     this.create();
   }
 
+  make2(hexer) {
+    let group = new three.Group();
+    for (let i = 0; i < 12; i++) {
+      let c = hexer.korners[i];
+      group.add(
+        this._setPosV(
+          this._makeball(),
+          this._fromSpher(c.spherical[0], c.spherical[1])
+        )
+      );
+    }
+
+    for (let j = 0; j < 10; j++) {
+      let c = hexer.korners[j];
+      group.add(this._makePatch(
+        hexer,
+        j, c.rightCi, c.upCi
+      ));
+      group.add(this._makePatch(
+        hexer,
+        j, c.rightCi, c.downCi
+      ));
+    }
+
+    return group;
+  }
+
+  _makePatch(hexer, c0, c1, c2) {
+    // c0 is the start corner index
+    // c1 is the corner index to the right
+    // c2 is the corner index up (or down)
+    let group = new three.Group();
+
+    let a = hexer.korners[c0].spherical;
+    let b = hexer.korners[c2].spherical;
+    let c = hexer.korners[c1].spherical;
+    for (let f = 0.0; f < 0.8; f += 0.05) {
+      let sph = hexer.lerpPatch(a, b, c, 0, f);
+      group.add(
+        this._setPosV(
+          this._makeball(0.05, 0x00aa00),
+          this._fromSpher(sph[0], sph[1])
+        )
+      );
+      sph = hexer.lerpPatch(a, b, c, f, 0);
+      group.add(
+        this._setPosV(
+          this._makeball(0.05, 0xff0000),
+          this._fromSpher(sph[0], sph[1])
+        )
+      );
+      sph = hexer.lerpPatch(a, b, c, f, .5);
+      group.add(
+        this._setPosV(
+          this._makeball(0.05, 0x9900ff),
+          this._fromSpher(sph[0], sph[1])
+        )
+      );
+    }
+
+    return group;
+  }
+
   makeSchematicSphere(hexer) {
     let group = new three.Group();
     hexer.sphere.verts.forEach(v => {
@@ -72,18 +147,80 @@ export class Part1 {
       group.add(this.makeball(position));
     });
 
+
+
+    // JUNK
+    // add some other balls
+    function _spher(i) {
+      return hexer.sphere.spherical[i];
+    }
+    function _fromSpher(s) {
+      return new three.Vector3(
+          Math.cos(s[0]) * Math.cos(-s[1]),
+          Math.sin(s[0]),
+          Math.cos(s[0]) * Math.sin(-s[1]),
+      );
+    }
+
+    let w0 = _fromSpher(_spher(0));
+    let w2 = _fromSpher(_spher(2));
+    let w4 = _fromSpher(_spher(4));
+
+    let s0 = _spher(2);
+    let s2 = _spher(1);
+    let s4 = _spher(3);
+    let dx = [s0[0]-s2[0], s0[1]-s2[1]];
+    let dy = [s4[0]-s2[0], s4[1]-s2[1]];
+
+    function _lerp(fx, fy) {
+      let f = [
+        (dx[0]*fx + dy[0]*fy) + s2[0],
+        (dx[1]*fx + dy[1]*fy) + s2[1],
+      ];
+      return(_fromSpher(f));
+    }
+
+    for(let f = 0.1; f <1; f+=0.1) {
+      group.add(this.makeball(_lerp(f, 0), 0xff0000));
+      group.add(this.makeball(_lerp(0, f), 0x00ff00));
+    }
+
+    group.add(this.makeball(w0, 0x8844ff));
+    group.add(this.makeball(w2, 0x8844ff));
+    group.add(this.makeball(w4, 0x8844ff));
+
     return group;
   }
 
-  makeball(position) {
-    const geometry = new three.SphereGeometry(0.15);
-    const material = new three.MeshPhongMaterial({color: 0x996600,});
-    const circle = new three.Mesh(geometry, material);
-    this.myscene.res3.add(circle);
-    if (position) {
-      circle.position.set(position.x, position.y, position.z);
-    }
-    return circle;
+  _setPosV(obj, position) {
+    return position ?
+      this._setPos(obj, position.x, position.y, position.z) :
+      obj;
   }
 
+  _setPos(obj, x, y, z) {
+    obj.position.set(x, y, z);
+    return obj;
+  }
+
+  _fromSpher(theta, phi) {
+    return new three.Vector3(
+        Math.cos(phi) * Math.cos(-theta),
+        Math.sin(phi),
+        Math.cos(phi) * Math.sin(-theta),
+    );
+  }
+
+  _makeball(radius = 0.1, color = 0xaa3300) {
+    const geo = new three.SphereGeometry(radius);
+    const mat = new three.MeshPhongMaterial({color: color,});
+    const mesh = new three.Mesh(geo, mat);
+    this.myscene.res3.add(mesh);
+    return mesh;
+  }
+
+  makeball(position, color = 0x996600) {
+    const ball = this._makeball(0.15, color);
+    return this._setPosV(ball, position);
+  }
 }

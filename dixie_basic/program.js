40bdc2d0ee9b2b472a21876620cd93b5c691a8c3
Detmar <detmar@peterke.com>
2024-05-11 15:52:19 -0700

    settin css for dixie

--- dixie_basic/program.js
+++ dixie_basic/program.js
@@ -1,122 +1,134 @@
 export default class Program {
   constructor() {
     this.makeOuter();
+    this.makeInner1();
+    this.makeInnerContents();
+    this.makeFlexArea();
+  }
+
+  run() {
   }
 
   makeOuter() {
-    this.div = document.createElement('div');
-    this.div.classList.add('dixie-outer');
-    document.body.appendChild(this.div);
+    /* make an outer div. We want it to fill the view window. */
+    this.outer = document.createElement('div');
+    this.outer.classList.add('dixie-outer');
+    document.body.appendChild(this.outer);
   }
-  run() {
 
-
-    const header = document.createElement('h2');
-    header.textContent = 'Dixie basic';
-    this.div.appendChild(header);
-
-    const p1 = document.createElement('p');
-    p1.textContent = 'OK. This a simple page. With a few goals.  First, for display we want a tall orientation, even on wide screens. Second, we want an optional way of doing vertical overflow. Option A is that the rect expands vertically.  Option B is that the rect does not expand, but it allows for some element within it to have its own internal scroll Bar.';
-    this.div.appendChild(p1);
-
-    const p2 = document.createElement('p');
-    p2.textContent = 'Second, I want a few basic controls to look good on desktop, then secondarily on android firefox, then lastly on iPad safari.';
-    this.div.appendChild(p2);
-
-    // Create the text input box
-    const div2 = document.createElement('div');
-    this.div.appendChild(div2);
-    const textBox = document.createElement('textarea');
-    textBox.rows = 4;
-    textBox.style.width = '100%';
-    textBox.placeholder = 'Enter text here';
-    textBox.style.resize = 'vertical'; // Make the text box resizable
-    div2.appendChild(textBox);
-
-    // Create the "Add" button
-    const addButton = document.createElement('button');
-    addButton.textContent = 'Add';
-    addButton.addEventListener('click', () => {
-      // Empty callback function
-    });
-    this.div.appendChild(addButton);
-
-    // Create the "Clear" button
-    const clearButton = document.createElement('button');
-    clearButton.textContent = 'Clear';
-    clearButton.addEventListener('click', () => {
-      // Empty callback function
-    });
-    this.div.appendChild(clearButton);
-
-    // Create the horizontal divider
-    const divider = document.createElement('hr');
-    this.div.appendChild(divider);
-    // Create the checkbox element
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = 'fooCheckbox';
-    checkbox.checked = true;
-    const label = document.createElement('label');
-    label.htmlFor = 'fooCheckbox';
-    label.textContent = 'Foo me';
-
-    let self = this;
-    // Add an event listener for the checkbox
-    checkbox.addEventListener('change', function () {
-      self.setTextScroll(this.checked);
-    });
-
-    // Append the checkbox to the container
-    this.div.appendChild(checkbox);
-    this.div.appendChild(label);
-
-    // Create the text element
-    const textElement = document.createElement('div');
-    textElement.textContent = 'hi';
-    this.div.appendChild(textElement);
-
+  makeFlexArea() {
     this.textBlock = document.createElement('div');
     this.textBlock.className = 'text-block';
-// Update this.textBlock to preserve newlines
-this.textBlock.style.whiteSpace = 'pre-line';
+    // Update this.textBlock to preserve newlines
+    this.textBlock.style.whiteSpace = 'pre-line';
     this.textBlock.style.border = '1px solid black';
-this.setTextScroll(checkbox.checked);
+    this.setTextScroll(this.checkFlex.checked);
 
     // Create the slider element
     const slider = document.createElement('input');
     slider.type = 'range';
     slider.min = 0;
     slider.max = 100;
-    slider.value = 5;
+    slider.value = 50;
     slider.style.width = '100%'; // Set width to 100% of container width
     slider.style.maxWidth = '400px'; // Set max-w
 
-  this.setTextContents(slider.value);
+    this.setBottomTextContents(slider.value);
 
 
     // Add an event listener for the slider
+    let self = this;
     slider.addEventListener('input', function () {
       const value = parseInt(this.value);
-      self.setTextContents(value);
+      self.setBottomTextContents(value);
     });
 
     // Append the slider to the container
-    this.div.appendChild(slider);
+    this.outer.appendChild(slider);
 
     // Append the text element to the container
-    this.div.appendChild(this.textBlock);
+    this.outer.appendChild(this.textBlock);
   }
 
-  // Define the setTextBlock function
-  setTextContents(lines) {
+  makeInnerContents() {
+    /* Controls for the top half of the display */
+
+    /* Some header text */
+    const header = document.createElement('h2');
+    header.textContent = 'Dixie basic';
+    this.inner1.appendChild(header);
+
+    const p1 = document.createElement('p');
+    p1.textContent = 'OK. This a simple page. With a few goals.  First, for display we want a tall orientation, even on wide screens. Second, we want an optional way of doing vertical overflow. Option A is that the rect expands vertically.  Option B is that the rect does not expand, but it allows for some element within it to have its own internal scroll Bar.';
+    this.inner1.appendChild(p1);
+
+    const p2 = document.createElement('p');
+    p2.textContent = 'Second, I want a few basic controls to look good on desktop, then secondarily on android firefox, then lastly on iPad safari.';
+    this.inner1.appendChild(p2);
+
+    /* A text area */
+    const textBox = document.createElement('textarea');
+    textBox.rows = 4;
+    textBox.style.width = '100%';
+    textBox.placeholder = 'Enter text here';
+    textBox.style.resize = 'vertical'; // Make the text box resizable
+    this.inner1.appendChild(textBox);
+
+    /* Add ands Clear buttons for the text area */
+    const addButton = document.createElement('button');
+    addButton.textContent = 'Add';
+    addButton.addEventListener('click', () => {
+      let currentText = textBox.value;
+      currentText += `Length was ${textBox.value.length}. Appending...\n`;
+      textBox.value = currentText;
+      textBox.scrollTop = 9999999;
+    });
+    this.inner1.appendChild(addButton);
+
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear';
+    clearButton.addEventListener('click', () => {
+      textBox.value='';
+    });
+    this.inner1.appendChild(clearButton);
+
+    const divider = document.createElement('hr');
+    this.inner1.appendChild(divider);
+
+    /* A checbox to control the flex property of the bottom part */
+    this.checkFlex = document.createElement('input');
+    this.checkFlex.type = 'checkbox';
+    this.checkFlex.id = 'fooCheckbox';
+    this.checkFlex.checked = true;
+    const label = document.createElement('label');
+    label.htmlFor = 'fooCheckbox';
+    label.textContent = 'Use flex for lower text';
+
+    let self = this;
+    this.checkFlex.addEventListener('change', function () {
+      self.setTextScroll(this.checked);
+    });
+
+    this.inner1.appendChild(this.checkFlex);
+    this.inner1.appendChild(label);
+  }
+
+  makeInner1() {
+    /* A div for the top half, so that its contents don't flex */
+    this.inner1 = document.createElement('div');
+    this.outer.appendChild(this.inner1);
+  }
+
+  setBottomTextContents(lines) {
+    /* set N lines of text for the bottom text area */
     const linesArray = Array.from({ length: lines }, (_, index) => `Line ${index + 1} of ${lines}`);
     this.textBlock.textContent = linesArray.join('\n');
   }
 
   setTextScroll(scroll) {
+    /* Set the flex behavior of the bottom text area */
     this.textBlock.classList.toggle('dixie-flex', scroll);
-    this.div.classList.toggle('dixie-outer-flex', scroll);
+    this.outer.classList.toggle('dixie-outer-flex', scroll);
   }
 
 }

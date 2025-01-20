5a689ad787ce26211e8c9cd244f7031ad75a66b7
Detmar <detmar@peterke.com>
2025-01-19 18:22:03 -0800

    draw stuff

--- glinda/glinda.js
+++ glinda/glinda.js
@@ -15,8 +15,23 @@ export default class Glinda {
     this.context.fillRect(0, 0, canvas.width, canvas.height);
 
     // Draw a square with size based on dt
-    const size = dt * 1000;
+    const size = dt * 10000;
     this.context.fillStyle = 'black';
     this.context.fillRect(0, 0, size, size);
+
+    const x = Math.floor(canvas.width / 2);
+    const y = Math.floor(canvas.height / 2);
+    this.drawTile(x, y, {});
+  }
+
+  drawTile(x, y, tile) {
+    const width = 64;
+    const height = 48;
+    const canvasHeight = this.context.canvas.height;
+
+    const adjustedY = canvasHeight - y - height;
+
+    this.context.fillStyle = 'cyan';
+    this.context.fillRect(x, y, width, height);
   }
 }
